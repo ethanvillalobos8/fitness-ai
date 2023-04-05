7229c553387462d58b2abc0ba61d72601eb264aa
Ethan <ethan.villalobos8@gmail.com>
2023-04-05 13:09:11 -0500

    Update apos errors

--- app/calculator/calories/page.js
+++ app/calculator/calories/page.js
@@ -7,14 +7,14 @@ export default function bmr() {
                 <h1 className="text-4xl font-bold text-zinc-50">Caloric Intake</h1>
                 <div className='flex'>
                     <p className='text-justify text-zinc-50'>
-                        Caloric intake refers to the total number of calories a person consumes in a day. It's an important concept when 
+                        Caloric intake refers to the total number of calories a person consumes in a day. It&apos;s an important concept when 
                         it comes to weight management because if you consume more calories than your body burns, you will gain weight, and 
                         if you consume fewer calories than your body burns, you will lose weight.
                         <br></br><br></br>
                         Maintaining a caloric deficit means that you are consuming fewer calories than your body burns in a day. This is 
-                        important for weight loss because your body needs to burn stored fat for energy when it doesn't have enough calories 
+                        important for weight loss because your body needs to burn stored fat for energy when it doesn&apos;t have enough calories 
                         from food. By consistently maintaining a caloric deficit over time, you can lose weight and improve your overall health. 
-                        However, it's important to maintain a balanced diet and ensure that you are still consuming enough nutrients to support 
+                        However, it&apos;s important to maintain a balanced diet and ensure that you are still consuming enough nutrients to support 
                         your body's needs.
                     </p>
                 </div>
